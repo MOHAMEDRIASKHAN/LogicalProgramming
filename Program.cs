1fe9a94318190ac715d2af8ba157bba5fbb27473
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-11 20:58:31 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace LogicalProgramming
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //FibonacciSeries.DisplayFibonacciSeries();
+            //PrefectNumber.DisplayPerfectNumber();
+            //ReverseNumber.DisplayReverseNumber();
+            //PrimeNumber.DisplayPrimeNumber();
+            //StopWatch.DisplayStopWatch();
+            CouponNumber.DisplayCouponNumber();
+        }
+    }
+}
